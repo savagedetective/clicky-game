97fcca846cc7752c6069daf8a877a325d8cfee9b
Ritchie Garcia <detectives.salvajes@protonmail.com>
2019-04-27 17:47:40 -0700

    components now seem to work

---
+++ src/components/Jumbotron/Jumbotron.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Jumbotron = () => (
+
+    <nav className="">
+        <p>hello</p>
+    </nav>
+
+);
+
+export default Jumbotron
---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Nav = () => (
+
+    <nav className="">
+        <p>hello</p>
+    </nav>
+
+);
+
+export default Nav
