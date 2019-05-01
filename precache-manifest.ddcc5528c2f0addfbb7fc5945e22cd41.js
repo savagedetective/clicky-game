6ee80958b088900289b4a94e9fddbc353a03321d
Ritchie Garcia <detectives.salvajes@protonmail.com>
2019-04-30 19:37:47 -0700

    Updates

---
+++ precache-manifest.ddcc5528c2f0addfbb7fc5945e22cd41.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b815ce865c3113fb1a7fd408ace2eca",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "43261e0d6f3e1c584878",
+    "url": "/clicky-game/static/css/main.5e4b436d.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/clicky-game/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "43261e0d6f3e1c584878",
+    "url": "/clicky-game/static/js/main.37c60472.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  }
+]);
